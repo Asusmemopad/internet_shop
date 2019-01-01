ba4cf2d23b935ab969792523d8a666f4638c53f8
Юрій <89320dota@gmail.com>
2019-01-01 09:48:45 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/model/Goods.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Goods {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String code;
+    private String name;
+    private Long price;
+}
