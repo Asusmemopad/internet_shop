6327fed41c8f1e54274521619389d9c4989e7726
Юрій <89320dota@gmail.com>
2019-01-03 07:41:44 +0200

    add validation

--- src/main/java/com/example/demo/controllers/GoodsController.java
+++ src/main/java/com/example/demo/controllers/GoodsController.java
@@ -5,9 +5,13 @@ import com.example.demo.services.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.UnsupportedEncodingException;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/goods")
@@ -17,10 +21,20 @@ public class GoodsController {
     private GoodsService goodsService;
 
     @PostMapping("")
-    public ResponseEntity<Goods> createNewGoods(@RequestBody Goods goods){
-        System.out.println("goods " + goods);
+    public ResponseEntity<?> createNewGoods(@Valid @RequestBody Goods goods, BindingResult result){
+
+        if (result.hasErrors()){
+
+            Map<String, String> errorMap = new HashMap<>();
+
+            for (FieldError error: result.getFieldErrors()){
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
+        goodsService.saveGoods(goods);
         return new ResponseEntity<Goods>(goods, HttpStatus.CREATED);
     }
-
-
 }
--- src/main/java/com/example/demo/model/Goods.java
+++ src/main/java/com/example/demo/model/Goods.java
@@ -3,9 +3,12 @@ package com.example.demo.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 @Data
@@ -14,9 +17,11 @@ public class Goods {
     @Id
     @GeneratedValue
     private Long id;
+    @NotBlank(message = "goods code is required")
+    @Size(min = 4, message = "please use minimum 4 characters")
+    @Column(updatable = false, unique = true)
     private String code;
+    @NotBlank(message = "goods name is required")
     private String name;
     private Long price;
-
-
 }
